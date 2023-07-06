98b9f155be350556ca58d66dc69467cb0cf81488
camila <camilasconceicao@hotmail.com>
2023-07-06 09:36:26 -0300

    final

--- script1.js
+++ script1.js
@@ -1,19 +1,55 @@
-import numeroPorExtenso from 'numero-por-extenso';
+function converterNumero() {
+  const numero = parseInt(document.getElementById('numero').value);
+  const porExtenso = numeroPorExtenso(numero);
+  document.getElementById('resultado').textContent = porExtenso;
+}
 
-const numeroPorExtenso = require('numero-por-extenso');
 
-//const numeroExtenso = numeroPorExtenso.porExtenso(55);
-//console.log(numeroExtenso);  // Saída: "quarenta e dois"
+function espacamento() {
+  const espaco = document.getElementById('resultado');
+  espaco.innerHTML = exemplo.numero.join('e ');
+}
 
-function converter() {
-  debugger
-  let numero = parseInt(document.getElementById('numero').value);
 
-  if (isNaN(numero) || numero < 0 || numero > 9999) {
-    document.getElementById('resultado').value = alert('Número inválido.');
-    return;
+function numeroPorExtenso(numero) {
+  const unidades = ['', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove'];
+  const especiais = ['', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove'];
+  const dezenas = ['', 'dez', 'vinte', 'trinta', 'quarenta', 'cinquenta', 'sessenta', 'setenta', 'oitenta', 'noventa'];
+  const centenas = ['', 'cento', 'duzentos', 'trezentos', 'quatrocentos', 'quinhentos', 'seiscentos', 'setecentos', 'oitocentos', 'novecentos'];
+
+  if (numero === 0) {
+    return 'zero';
   }
 
-  let porExtenso = numeroPorExtenso.porExtenso(numero);
-  document.getElementById('resultadoExtenso').value = porExtenso;
+  let porExtenso = '';
+  document.getElementById('numero').value = ""
+
+  // milhar
+  const unidadeDeMilhar = Math.floor(numero / 1000);
+  if (unidadeDeMilhar > 0) {
+    porExtenso += unidades[unidadeDeMilhar] + ' mil ';
+  }
+
+  // centena
+  const centena = Math.floor((numero % 1000) / 100);
+  if (centena > 0) {
+    porExtenso += centenas[centena] + ' ';
+  }
+
+  // dezena e a unidade
+  const dezenaUnidade = numero % 100;
+  if (dezenaUnidade >= 11 && dezenaUnidade <= 19) {
+    porExtenso += especiais[dezenaUnidade - 10];
+  } else {
+    const dezena = Math.floor(dezenaUnidade / 10);
+    const unidade = dezenaUnidade % 10;
+    if (dezena > 0) {
+      porExtenso += dezenas[dezena] + ' ';
+    }
+    if (unidade > 0) {
+      porExtenso += unidades[unidade];
+    }
+  }
+
+  return porExtenso.trim('');
 }
